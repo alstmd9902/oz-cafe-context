429799ec1798d52b6952820ed2a42842621a8beb
Seungyeon Oh <alstmd9902@naver.com>
2026-01-06 23:59:35 +0900

    docs: 함수인자 넘기기 관련 질문 추가

--- src/context/cartContext.jsx
+++ src/context/cartContext.jsx
@@ -6,10 +6,20 @@ const cartContext = createContext();
 export function CartProvider({ children }) {
   const [cart, setCart] = useState([]);
 
+  /* 질문:
+   * addToCart(removeFromCart 도 동일)를 쓰는 건 알겠는데,
+   * 왜 그냥 addToCart()만 부르는 게 아니라
+   * options, quantity, modalMenu.id를
+   * 굳이 괄호 안에 넣어서 같이 보내야 하는지 모르겠습니다.
+   * 왜 OrderModal에서 만들어서 넘겨줘야 하는 건지 이해가 안됩니당 ...
+   */
+
+  //장바구니 추가하는 함수
   const addToCart = (options, quantity, id) => {
     setCart([...cart, { options, quantity, id }]);
   };
 
+  //삭제 하는 함수
   const removeFromCart = (id) => {
     setCart(cart.filter((el) => el.id !== id));
   };
